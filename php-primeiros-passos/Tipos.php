ab5290cc5d42762402ff45b939978f41261598db
Vinicius Batista <viniciusbatista023@gmail.com>
2020-01-06 08:30:59 -0300

    working with variables types in php

---
+++ php-primeiros-passos/Tipos.php
@@ -0,0 +1,13 @@
+<?php
+
+$idade = 25;
+$salario = 1000.32;
+$divisao = 10 / 3;
+$texto = 'texto';
+$booleano = true;
+
+echo gettype($idade) . PHP_EOL;
+echo gettype($salario) . PHP_EOL;
+echo gettype($divisao) . PHP_EOL;
+echo gettype($texto) . PHP_EOL;
+echo gettype($booleano) . PHP_EOL;
